c461ce9798e063c76994dc6108ecdf3696b548f1
Wladzio <bucarator@gmail.com>
2015-03-18 13:49:04 +0100

    2nd

--- app/src/main/java/com/s3ns3i/degejm/CombatResolutionDataFromServer.java
+++ app/src/main/java/com/s3ns3i/degejm/CombatResolutionDataFromServer.java
@@ -1,21 +1,35 @@
 package com.s3ns3i.degejm;
 
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.ListView;
 
 public class CombatResolutionDataFromServer extends AsyncTask<String, String, String> {
-	private String player_ID_;
-	private String target_ID_;
-	private String damageAmount_;
-	private String damageType_;
-	private Boolean onlineState_;
 
+    private JSONParser jParser = new JSONParser();
+    private String []dataTables ={"attacker_id","defender_id","meele_attack","magic_attack","online_state"};
+    private static String url = "http://wfisfantasy.16mb.com/";
 
+    private Context context;
+    private ListView LVenemies;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+    }
 
 
     @Override
     protected String doInBackground(String... params) {
         return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
     }
 
 
